404f10346eb8262eee35a59ea6550c3b0369bd43
xX_HellSushi.DøøM_Xx <inferuskaiser09@gmail.com>
2026-09-16 16:48:17 -0600

    Feature/navigation (#9)
    * chore: configurar .gitignore robusto para el equipo
    
    * feat(navigation): configuracion de navhost y rutas seguras para la app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
+    // Navegación en Jetpack Compose
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
